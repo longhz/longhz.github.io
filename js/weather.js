6ca3da0328c55f8066262bb0d4161883c8bc8de4
longhz <1084045717@qq.com>
2022-07-26 11:58:54 +0800

    Site updated: 2022-07-26 11:58:49

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "4D254D",
+    "citySize": "16",
+    "aqiColor": "F291F2",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "30",
+    "top": "-15",
+    "key": "cc796ceabab445aebd8146a0da3f5b13"
+  }
+}
